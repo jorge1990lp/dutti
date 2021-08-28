867264660b5032df1a0e5a5734dcfdfa52974e59
Jorge Ortega Castellano <jorge1990lp@gmail.com>
2021-08-28 17:12:00 +0100

    Corrección de error een el archivo principal.module.ts. Se declara PrincipalComponent y se exporta el resto de componentes.

--- src/app/modules/principal/principal.module.ts
+++ src/app/modules/principal/principal.module.ts
@@ -8,19 +8,27 @@ import { ShipsComponent } from './components/ships/ships.component';
 import { PageOneComponent } from './components/page-one/page-one.component';
 import { PageTwoComponent } from './components/page-two/page-two.component';
 import { ShipsDetailsComponent } from './components/ships-details/ships-details.component';
+import { PrincipalComponent } from './principal.component';
 
 @NgModule({
   declarations: [
     ShipsComponent,
     ShipsDetailsComponent,
     PageOneComponent,
-    PageTwoComponent
+    PageTwoComponent,
+    PrincipalComponent
   ],
   imports: [
     CommonModule,
     PrincipalComponentsRoutingModule,
     HttpClientModule,
     NgxPaginationModule
+  ],
+  exports: [
+    ShipsComponent,
+    ShipsDetailsComponent,
+    PageOneComponent,
+    PageTwoComponent
   ]
 })
 export class PrincipalModule { }
